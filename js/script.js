e8f1aef0b65394070075370904620713fe3f370f
Echoloca <echoloca@gmail.com>
2023-10-02 11:47:49 +0330

    js commands for likes added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 let heartIcon = document.querySelector(".icon-box .bx-heart");
 let heartIconRed = document.querySelector(".icon-box .fa-heart");
+let likes = document.querySelector('.icons-likes .likes p');
+let count = 0;
+
 
 heartIcon.addEventListener("click", function () {
     heartIcon.style.display = "none";
     heartIconRed.style.display = "block";
+    count++;
+    likes.innerHTML = count + " likes";
+    likes.style.display = 'block';
+
 });
 
 heartIconRed.addEventListener("click", function () {
     heartIconRed.style.display = "none";
     heartIcon.style.display = "block";
+    count--;
+    if (count < 1) {
+        likes.style.display = 'none';
+    }
+
+    else if (count > 1) {
+        likes.innerHTML = count + " likes";
+    }
 });
+
+
+
+
